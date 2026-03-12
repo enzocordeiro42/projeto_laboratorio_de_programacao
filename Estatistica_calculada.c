153166bc395cb0afa59de7137d6370a5fd7b593d
Gustav <gustavoscdasilva1201@gmail.com>
2026-03-12 08:50:06 -0300

    Contatos por alfabeto

---
+++ Estatistica_calculada.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <string.h>
+#include "contatos.h"
+
+void estatistica(struct contato *p)
+{
+
+int i = 0;
+int a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z;
+
+ do {
+        if (p->nome[0] == 'a' || p->nome[0] == 'A') {
+           a++;
+        }
+        if (p->nome[0] == 'b' || p->nome[0] == 'B') {
+           b++;
+        }
+        if (p->nome[0] == 'c' || p->nome[0] == 'C') {
+           c++;
+        }
+        if else(p->nome[0] == 'd'){
+            d++
+        }
+        if else(p->nome[0] == 'f'){
+            e++
+        }
+        if else (strcmp(contato[i], p->nome) == f){
+            f++
+        }
+        if else (strcmp(contato[i], p->nome) == g){
+            g++
+        }
+         if else (strcmp(contato[i], p->nome) == h){
+            h++
+        }
+         if else (strcmp(contato[i], p->nome) == i){
+            i++
+        }
+         if else (strcmp(contato[i], p->nome) == j){
+            j++
+        }
+        if else (strcmp(contato[i], p->nome) == k){
+            k++
+        }
+        if else (strcmp(contato[i], p->nome) == l){
+            l++
+        }
+        if else (strcmp(contato[i], p->nome) == m){
+            m++
+        }
+        if else (strcmp(contato[i], p->nome) == o){
+            o++
+        }
+        if else (strcmp(contato[i], p->nome) == p){
+            p++
+        }
+        if else (strcmp(contato[i], p->nome) == q){
+            q++
+        }
+        if else (strcmp(contato[i], p->nome) == r){
+            r++
+        }
+        if else (strcmp(contato[i], p->nome) == s){
+            s++
+        }
+        if else (strcmp(contato[i], p->nome) == u){
+            u++
+        }
+        if else (strcmp(contato[i], p->nome) == v){
+            v++
+        }
+        if else (strcmp(contato[i], p->nome) == w){
+            w++
+        }
+        if else (strcmp(contato[i], p->nome) == x){
+            x++
+        }
+        if else (strcmp(contato[i], p->nome) == y){
+            y++
+        }
+        if else (strcmp(contato[i], p->nome) == z){
+            z++
+        }
+
+        p++;
+        i++;
+    } while (p->nome[0] != '\000');
+
+
+
+
+
+};
--- contatos.h
+++ contatos.h
@@ -6,3 +6,4 @@ void listar_contatos(struct contato *p, int n);
 void buscar_contato(struct contato *p);
 void editar_contato(struct contato *p);
 void apagar_contatos(struct contato *p);
+void estatistica(struct contato *p);
